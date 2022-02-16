39acb1e9e01583a26fe4f1b3d9e4c9e305c3c166
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-17 00:04:36 +0100

    Update formula to release version 0.4.0.

--- Formula/timer-for-python.rb
+++ Formula/timer-for-python.rb
@@ -3,11 +3,11 @@ class TimerForPython < Formula
 
   desc "Makes it easy to measure time and performance of Python programs"
   homepage "https://github.com/jakob-bagterp/timer-for-python"
-  url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.3.4/timer-for-python-0.3.4.tar.gz"
-  sha256 "ab64da1a76157f56595fc972016fc260d5467e5f66e3dce5b4fb20fd002da322"
+  url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.4.0/timer-for-python-0.4.0.tar.gz"
+  sha256 "bcc128519115decf2f36ef5c66ede4aa190b61933b45bb957bdc5d4495af331b"
   license "MIT"
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
