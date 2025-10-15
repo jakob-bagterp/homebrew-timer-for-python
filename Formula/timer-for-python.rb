31dbf43b316273bb33e86388381b9367c7799d5e
Jakob Bagterp <25110864+jakob-bagterp@users.noreply.github.com>
2025-10-15 19:11:50 +0200

    Updated formula to version 0.9.5

--- Formula/timer-for-python.rb
+++ Formula/timer-for-python.rb
@@ -3,8 +3,8 @@ class TimerForPython < Formula
 
   desc "Makes it easy to measure time and performance of Python programs"
   homepage "https://github.com/jakob-bagterp/timer-for-python"
-  url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.9.4/timer_for_python-0.9.4.tar.gz"
-  sha256 "1202f0c77c2824142fee4b7fee8658926f1814f6d8fb33ec98b16a6ecea95c65"
+  url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.9.5/timer_for_python-0.9.5.tar.gz"
+  sha256 "eeb37899bb19b8b235e41bc0f8cedd96d8773e1e646039f6bd423d7e9646c3ba"
   license "MIT"
 
   depends_on "python@3.10"
